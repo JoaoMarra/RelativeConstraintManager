2087650327b5aa39fcf40dc3590a70c552b431a1
João Gabriel <silvamarraster@gmail.com>
2017-09-30 13:29:41 -0300

    created

---
+++ RelativeConstraintManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RelativeConstraintManager
+//
+//  Created by João Gabriel Silva Marra on 30/09/17.
+//  Copyright © 2017 marraWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RelativeConstraintManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RelativeConstraintManager
+//
+//  Created by João Gabriel Silva Marra on 30/09/17.
+//  Copyright © 2017 marraWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
